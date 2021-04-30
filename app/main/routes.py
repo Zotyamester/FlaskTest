9a2b9869d4ac28139f1918bf609dbe8a6a1d1b3a
Zotyamester <szatmary.zoltan1222@gmail.com>
2021-04-30 23:04:56 +0200

    Fix the ajax query of posts

--- app/main/routes.py
+++ app/main/routes.py
@@ -15,13 +15,12 @@ def before_request():
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
 
-@bp.route('/posts/<int:page>')
-def posts(page):
-    username = request.args.get('username', '', type=str)
-    base = Post.query
-    if username != '':
-        base = User.query.filter_by(username=username).first_or_404().posts
-    posts = base.order_by(Post.timestamp.desc()).paginate(page, current_app.config['POSTS_PER_PAGE'], False)
+
+def order_and_paginate_posts(posts, page):
+    return posts.order_by(Post.timestamp.desc()).paginate(page, current_app.config['POSTS_PER_PAGE'], False)
+
+
+def posts_to_json(posts):
     return jsonify({
         'posts': [
             {
@@ -31,6 +30,7 @@ def posts(page):
             } for post in posts.items]
     })
 
+
 @bp.route('/', methods=['GET', 'POST'])
 @bp.route('/index', methods=['GET', 'POST'])
 def index():
@@ -38,31 +38,62 @@ def index():
         return redirect(url_for('main.explore'))
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(title=form.title.data, body=form.post.data, author=current_user)
+        post = Post(title=form.title.data,
+                    body=form.post.data, author=current_user)
         db.session.add(post)
         db.session.commit()
         flash('Your post is now live!')
         return redirect(url_for('main.index'))
-    posts = current_user.followed_posts().paginate(1, current_app.config['POSTS_PER_PAGE'], False)
+    posts = current_user.followed_posts().paginate(
+        1, current_app.config['POSTS_PER_PAGE'], False)
     return render_template('index.html', title='Home', form=form, posts=posts.items)
+
+
+@bp.route('/posts', methods=['GET', 'POST'])
+@bp.route('/index/posts', methods=['GET', 'POST'])
+def index_posts():
+    page = request.args.get('page', 1, type=int)
+    posts = current_user.followed_posts()
+    return posts_to_json(order_and_paginate_posts(posts, page))
+
 
 @bp.route('/explore')
 def explore():
-    posts = Post.query.order_by(Post.timestamp.desc()).paginate(1, current_app.config['POSTS_PER_PAGE'], False)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+        1, current_app.config['POSTS_PER_PAGE'], False)
     return render_template('index.html', title='Explore', posts=posts.items)
+
+
+@bp.route('/explore/posts')
+def explore_posts():
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query
+    return posts_to_json(order_and_paginate_posts(posts, page))
+
 
 @bp.route('/user/<username>')
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = user.posts.order_by(Post.timestamp.desc()).paginate(1, current_app.config['POSTS_PER_PAGE'], False)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
+        1, current_app.config['POSTS_PER_PAGE'], False)
     form = EmptyForm()
     return render_template('user.html', title=user.username, user=user, posts=posts.items, form=form)
+
+
+@bp.route('/user/<username>/posts')
+def user_posts(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    page = request.args.get('page', 1, type=int)
+    posts = user.posts
+    return posts_to_json(order_and_paginate_posts(posts, page))
+
 
 @bp.route('/user/<username>/popup')
 def user_popup(username):
     user = User.query.filter_by(username=username).first_or_404()
     form = EmptyForm()
     return render_template('_user.html', user=user, form=form)
+
 
 @bp.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
@@ -77,6 +108,7 @@ def edit_profile():
         db.session.commit()
         flash('Your changes have been saved.')
     return render_template('edit_profile.html', title='Edit Profile', form=form)
+
 
 @bp.route('/follow/<username>', methods=['POST'])
 @login_required
@@ -96,6 +128,7 @@ def follow(username):
         return redirect(url_for('main.user', username=username))
     return redirect(url_for('main.index'))
 
+
 @bp.route('/unfollow/<username>', methods=['POST'])
 @login_required
 def unfollow(username):
@@ -114,19 +147,22 @@ def unfollow(username):
         return redirect(url_for('main.user', username=username))
     return redirect(url_for('main.index'))
 
+
 @bp.route('/send_message/<recipient>', methods=['GET', 'POST'])
 @login_required
 def send_message(recipient):
     user = User.query.filter_by(username=recipient).first_or_404()
     form = MessageForm()
     if form.validate_on_submit():
-        msg = Message(author=current_user, recipient=user, body=form.message.data)
+        msg = Message(author=current_user, recipient=user,
+                      body=form.message.data)
         db.session.add(msg)
         user.add_notification('unread_message_count', user.new_messages())
         db.session.commit()
         flash('Your message has been sent.')
         return redirect(url_for('main.user', username=recipient))
     return render_template('send_message.html', title='Send message', form=form, recipient=recipient)
+
 
 @bp.route('/messages')
 @login_required
@@ -138,9 +174,12 @@ def messages():
     messages = current_user.messages_received.order_by(
         Message.timestamp.desc()).paginate(
             page, current_app.config['POSTS_PER_PAGE'], False)
-    next_url = url_for('main.messages', page=messages.next_num) if messages.has_next else None
-    prev_url = url_for('main.messages', page=messages.prev_num) if messages.has_prev else None
+    next_url = url_for(
+        'main.messages', page=messages.next_num) if messages.has_next else None
+    prev_url = url_for(
+        'main.messages', page=messages.prev_num) if messages.has_prev else None
     return render_template('messages.html', messages=messages.items, next_url=next_url, prev_url=prev_url)
+
 
 @bp.route('/notifications')
 @login_required
